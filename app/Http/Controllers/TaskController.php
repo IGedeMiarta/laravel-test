7bc000e3ba4828fd1bbee1d2ca3615fe7ebc585b
miartayasa <miartayasa10@gmail.com>
2023-11-04 01:53:05 +0800

    done: laravel eloquent-relationship test

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,17 +10,17 @@ class TaskController extends Controller
     public function index()
     {
         $tasks = Task::with('user')->paginate();
-
         return view('tasks.index', compact('tasks'));
     }
 
     public function store(Request $request)
     {
         // TASK: find out why this sentence fails, and fix it in Eloquent Model
-        auth()->user()->tasks()->create([
-            'name' => $request->name
+        
+        Task::create([
+            'name' => $request->name,
+            'users_id' => auth()->id()
         ]);
-
         return 'Success';
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-
+        $users = User::has('projects')->get();
         return view('users.index', compact('users'));
     }
 
     public function show(User $user)
     {
+        // $user = User::with('comments')->where('username',$user)->get();
+        $user->with('comments')->get();
         return view('users.show', compact('user'));
     }
 }
